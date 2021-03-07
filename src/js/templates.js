fbc6c3ba932ff48f182e94f7ef0df5e4fc1d4c54
Maksym Balaba <jsmaks26@gmail.com>
2021-03-07 13:32:33 +0200

    dz-12

--- src/js/templates.js
+++
@@ -1,8 +0,0 @@
-import itemsTemplate from '../tempates/gallery-items.hbs'
-import menu from '../menu.json'
-
-const menuRef = document.querySelector('.js-menu')
-const menuGallery = itemsTemplate(menu);
-menuRef.insertAdjacentHTML('beforeend', menuGallery)
-
-console.log(menuGallery);
